c15a0d8c8be0acebf01f09e455efc978d6ffdcca
0xff3232 <150560315+0xff3232@users.noreply.github.com>
2023-12-02 03:07:37 +0000

    sortedSquares

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
 # main is used for current problem, move to directory once complete :-)
-# test
-
-nums = [1, 5, 2, 9, 3]
-nums2 = [-7, -3, 2, 3, 11]
-
-def sortedSquares(arr):
-    r = arr.copy()
-    r.sort()
-    return r
-    
-    
-    
-
-print(sortedSquares(nums))
-print(sortedSquares(nums2))
